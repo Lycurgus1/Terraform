042581487bd4224da4d5224a2c89db47ba0765bb
Lycurgus1 <max.palmer.official@outlook.com>
2020-08-21 10:04:05 +0100

    implemeneting using variables

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@
 # Provider of cloud services
 provider "aws" {
   # Define region to create instance in
-  region  = "eu-west-1"
+  region  = var.region
 }
 
 # Create an instance
 resource "aws_instance" "max_app_terraform" {
   # Defining ami image
-  ami           = "ami-0ae4f9fefc0735a61"
+  ami           = var.app_ami_id
   # Defining instance type to be created
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   # Deciding on public IP
   associate_public_ip_address = true
   # Naming instance
--- variables.tf
+++ variables.tf
@@ -1,11 +1,16 @@
 # Creating reusable variables to use in main.tf
 
-#app_instance: aws_instance
+variable "app_ami_id" {
+    type = string
+    default = "ami-0ae4f9fefc0735a61"
+}
 
-#name: eng67_max_terraform_ec2
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
 
-#variable "app_ami_id" {
-#    type = string
-#    default = "ami-0ae4f9fefc0735a61"
-#}
-
+variable "region" {
+    type = string
+    default = "eu-west-1"
+}
